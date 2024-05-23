0285d07ab6b5d66ae3d2217dbc3b8213837be234
Edward <78192184+F8TGT@users.noreply.github.com>
2024-05-23 12:05:57 -0600

    Show Books was created
    *Added Search functionality.
    *Added the tabs and created queries to display the information from the DB.
    *Created the Show individual books view.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,7 +13,21 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
-        $books = Book::when($title, fn ($query, $title) => $query->title($title))->get();
+        $filter = $request->input('filter', '');
+
+        $books = Book::when($title, fn ($query, $title) => $query->title($title));
+
+        $books = match($filter){
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn()=> $books->get());
+
         return view('books.index', ['books' => $books]);
     }
 
@@ -36,9 +50,15 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view(
+            'books.show', [
+                'book' => $book ->load([
+                    'reviews' => fn($query) => $query->latest()
+                    ])
+                ]
+            );
     }
 
     /**

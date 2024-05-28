0df3950d444ea6015996a8e8aee1733b214b851b
Edward <78192184+F8TGT@users.noreply.github.com>
2024-05-28 10:37:45 -0600

    Success Review Added Alert
    Added an alert to display a success message when review is added successfully.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Book Reviews</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.0/dist/cdn.min.js"></script>
 
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
@@ -52,6 +53,22 @@
 </head>
 
 <body class="container mx-auto mt-10 mb-10 max-w-3xl">
+    @if (session()->has('success'))
+        <div x-data="{ flash: true }">
+            <div x-show="flash"
+                class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+                role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div>{{ session('success') }}</div>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        @click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
+        </div>
+    @endif
     @yield('content')
 </body>
 
